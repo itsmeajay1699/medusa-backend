07b0dbb7acedd7597e8f111b43171713eb1dde78
itsmeajay1699 <ajayroy03377@gmail.com>
2025-08-15 13:33:56 +0530

    minor changes

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,5 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
@@ -11,15 +10,50 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
+
 
   plugins: [
     {
-      resolve: "@medusajs/file-local",
+      resolve: `@medusajs/file-s3`,
       options: {
-        upload_dir: "uploads"
-      }
-    }
-  ]
+        s3_url: process.env.S3_URL,
+        bucket: process.env.S3_BUCKET,
+        region: process.env.S3_REGION,
+        access_key_id: process.env.S3_ACCESS_KEY_ID,
+        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        // optional
+        cache_control: process.env.S3_CACHE_CONTROL,
+        // optional
+        download_file_duration:
+          process.env.S3_DOWNLOAD_FILE_DURATION,
+      },
+    },
+    "medusa-plugin-razorpay-v2",
+    "@sgftech/payment-razorpay",
+  ],
+
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "medusa-plugin-razorpay-v2/providers/payment-razorpay/src",
+            id: "razorpay",
+            options: {
+              key_id: process.env.RAZORPAY_TEST_KEY_ID ?? process.env.RAZORPAY_ID,
+              key_secret: process.env.RAZORPAY_TEST_KEY_SECRET ?? process.env.RAZORPAY_SECRET,
+              razorpay_account: process.env.RAZORPAY_TEST_ACCOUNT ?? process.env.RAZORPAY_ACCOUNT,
+              webhook_secret: process.env.RAZORPAY_TEST_WEBHOOK_SECRET ?? process.env.RAZORPAY_WEBHOOK_SECRET,
+              automatic_expiry_period: 30,
+              manual_expiry_period: 20,
+              refund_speed: "normal",
+            },
+          },
+        ],
+      },
+    },
+  ],
 })
